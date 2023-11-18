e9ba4b9a56a69df6419b09593957f9be6621faae
Francisco Juan <juansoussev@gmail.com>
2023-11-18 15:06:18 -0300

    types, int, double, bool, and String

--- bin/alura_couse.dart
+++ bin/alura_couse.dart
@@ -1,3 +1,18 @@
 void main() {
-  print('Hello World');
+  int age = 16;
+  double height = 1.79;
+
+  var list = [];
+
+  for (int i = 1; i < age ; i ++) {
+    if (i % 2 == 0) {
+      list.add(i);
+    } else if (i % 2 == 1) {
+      print('impair');
+    }
+  }
+
+
+
+  print(list);
 }
